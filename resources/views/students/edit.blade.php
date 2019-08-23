65077f02dbb8369aa66d553297a13d64adeff37f
Hector Lopez <edu.rolon@gmail.com>
2019-08-23 10:27:10 -0400

    version 1.1

--- resources/views/students/edit.blade.php
+++ resources/views/students/edit.blade.php
@@ -6,7 +6,7 @@
     <div class="row">
         <div class="col-lg-12 margin-tb">
             <div class="pull-left">
-                <h2>Editar estudiantes detalle</h2>
+                <h2>Editar estudiante detalle</h2>
             </div>
             <div class="pull-right">
                 <a class="btn btn-primary" href="{{ route('students.index') }}"> Atras</a>
